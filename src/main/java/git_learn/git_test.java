7af35a0cc3e9e1ac023f3b2fb3ef5dfbcd5adf1f
Nwafu_LH <2385883104@qq.com>
2022-10-02 14:35:49 +0800

    hit-fix first commit

--- src/main/java/git_learn/git_test.java
+++ src/main/java/git_learn/git_test.java
@@ -4,5 +4,6 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello, world!!!");
         System.out.println("hello4");
+        System.out.println("国庆快乐");
     }
 }
