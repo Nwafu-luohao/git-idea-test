08d62bb2920e9885a17c16d769e6e46bfb8fbefb
Nwafu_LH <2385883104@qq.com>
2022-10-02 14:25:23 +0800

    second commit

--- src/main/java/git_learn/git_test.java
+++ src/main/java/git_learn/git_test.java
@@ -3,6 +3,5 @@ package git_learn;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello, world!!!");
-        System.out.println("lala");
     }
 }
