58b0ab38f937880a55e1f0991339453311fe20a7
Nwafu_LH <2385883104@qq.com>
2022-10-02 14:27:31 +0800

    third commit

--- src/main/java/git_learn/git_test.java
+++ src/main/java/git_learn/git_test.java
@@ -3,5 +3,6 @@ package git_learn;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello, world!!!");
+        System.out.println("hwllo3");
     }
 }
