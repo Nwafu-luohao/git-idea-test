4a8c56beb17d37117e7262727266bd9f708ef8e5
Nwafu_LH <2385883104@qq.com>
2022-10-02 14:42:30 +0800

    hit-fix third commit

--- src/main/java/git_learn/git_test.java
+++ src/main/java/git_learn/git_test.java
@@ -6,5 +6,6 @@ public class git_test {
         System.out.println("hello4");
         System.out.println("国庆快乐");
         System.out.println("git 1");
+        System.out.println("hot-fix git 1");
     }
 }
